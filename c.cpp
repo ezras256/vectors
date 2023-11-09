5667c96a4d8fa9375f9bf4f34b711e8a5113ec90
ezras256 <120516359+ezras256@users.noreply.github.com>
2023-11-08 20:26:32 -0600

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -3,19 +3,31 @@
 #include <iterator>
 #include <math.h>
 #include <tuple>
+#include <stdlib.h>
 
-std::tuple<float, float> getComponents(float vectorComponents[][2]) {
+std::tuple<float, float> getComponents(float vectorComponents[][2], int length) {
     float horizontal = 0;
     float vertical = 0;
-    int placeholder = sizeof(vectorComponents);
-    std::cout << placeholder << "\n";
-    for (int i = 0; i < placeholder; i++) {
-        horizontal = horizontal + cos(vectorComponents[i][0] * (M_PI/180));
-        vertical = vertical + sin(vectorComponents[i][1] * (M_PI/180));
+    
+    for (int i = 0; i < length; i++) {
+        horizontal = horizontal + (vectorComponents[i][0] * cos(vectorComponents[i][1] * (M_PI/180)));
+        vertical = vertical + (vectorComponents[i][0] * sin(vectorComponents[i][1] * (M_PI/180)));
     } 
 
 
-    return {horizontal, vertical};
+    return {roundf(horizontal * 10000) / 10000, roundf(vertical * 10000) / 10000};
+}
+
+float getResultantMagnitude(std::tuple<float, float> components) {
+    float a = std::get<0>(components);
+    float b = std::get<1>(components);
+    float resultant = sqrt(pow(a, 2) + pow(b, 2));
+    return roundf(resultant * 10000) / 10000;
+}
+
+float getAngle(std::tuple<float, float> components) {
+    float horizontal = std::get<0>(components);
+    float vertical = std::get<1>(components);
 }
 
 int main() {
@@ -32,7 +44,11 @@ int main() {
         std::cin >> vectorAngles[i][1];
     }
 
-    std::tuple<float, float> components = getComponents(vectorAngles);
-    std::cout << std::get<0>(components) << "\n";
-    std::cout << std::get<1>(components) << "\n";
+    std::tuple<float, float> components = getComponents(vectorAngles, numVectors);
+    
+    float resultantAngle = getAngle(components);
+    float resultantMagnitude = getResultantMagnitude(components);
+    std::cout << resultantMagnitude << "\n";
+
+
 }
